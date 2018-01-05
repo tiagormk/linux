8191c0bf4d7d7fcd76c913f06a7ba12bac53efd2
Tiago Muck <tmuck@uci.edu>
2018-01-04 17:04:28 -0800

    Defining ess_cause_emerg_events before it's used

--- arch/arm/mach-exynos/exynos-ss.c
+++ arch/arm/mach-exynos/exynos-ss.c
@@ -257,6 +257,13 @@ static unsigned int ess_phy_addr = 0;
 static unsigned int ess_virt_addr = 0;
 static unsigned int ess_size = 0;
 
+enum ess_cause_emerg_events {
+	CAUSE_INVALID_DUMP = 0x00000000,
+	CAUSE_KERNEL_PANIC = 0x00000001,
+	CAUSE_FORCE_DUMP   = 0x0000000D,
+	CAUSE_FORCE_REBOOT = 0x000000FF,
+};
+
 DEFINE_PER_CPU(struct exynos_ss_core_reg *, ess_core_reg);
 DEFINE_PER_CPU(struct exynos_ss_mmu_reg *, ess_mmu_reg);
 DEFINE_PER_CPU(enum ess_cause_emerg_events, ess_cause_emerg);
@@ -492,13 +499,6 @@ static inline void exynos_ss_hook_logbuf(const char *buf, u64 ts_nsec, size_t si
 	}
 }
 #endif
-
-enum ess_cause_emerg_events {
-	CAUSE_INVALID_DUMP = 0x00000000,
-	CAUSE_KERNEL_PANIC = 0x00000001,
-	CAUSE_FORCE_DUMP   = 0x0000000D,
-	CAUSE_FORCE_REBOOT = 0x000000FF,
-};
 
 static void exynos_ss_scratch_reg(unsigned int val)
 {
